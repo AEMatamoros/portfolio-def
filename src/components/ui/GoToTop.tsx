2349de9d5b238b374606403a786b375cf4e1cca1
AM10075 <49670332+AEMatamoros@users.noreply.github.com>
2025-02-24 11:44:36 -0600

    fix: removed comment

--- src/components/ui/GoToTop.tsx
+++ src/components/ui/GoToTop.tsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 
 //Scroll to top
 export const scrollToTop = () => {
-  console.log("first");
   window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
 };
 
